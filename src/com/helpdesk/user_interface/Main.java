aaf3392b2ac5459b2e3d86eb99192751979cc816
ruslan.hassonov@gmail.com <5p1d3rman246517482887>
2019-05-17 09:36:20 +0200

    none

--- src/com/helpdesk/user_interface/Main.java
+++ src/com/helpdesk/user_interface/Main.java
@@ -5,13 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
