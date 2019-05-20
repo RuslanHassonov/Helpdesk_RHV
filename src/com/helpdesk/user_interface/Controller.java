f3fe3130be5b8d52e57fdefa857d736eb3945bed
ruslan.hassonov@gmail.com <5p1d3rman246517482887>
2019-05-20 08:49:32 +0200

    none

--- src/com/helpdesk/user_interface/Controller.java
+++ src/com/helpdesk/user_interface/Controller.java
@@ -1,5 +1,26 @@
 package com.helpdesk.user_interface;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    private Button btn_Minimize;
+    @FXML
+    private Button btn_Close;
+
+    @FXML
+    private void minimizeButtonPressed(){
+        Stage stage =(Stage) btn_Minimize.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void closeButtonPressed(){
+        Platform.exit();
+    }
 
 }
